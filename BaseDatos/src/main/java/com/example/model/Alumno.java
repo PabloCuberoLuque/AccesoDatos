f3f811314964bedef373ca241f3f575da459d781
Pablo <123060435+PabloCuberoLuque@users.noreply.github.com>
2023-10-30 20:03:31 +0100

    Ejercicio añadir alumno BBDD

--- BaseDatos/src/main/java/com/example/model/Alumno.java
+++ BaseDatos/src/main/java/com/example/model/Alumno.java
@@ -1,10 +1,7 @@
 package com.example.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,9 +12,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "estudiantes")
 public class Alumno {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     String nombre;
     String apellidos;
